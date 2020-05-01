ec02ae0c052e685962525e71256b1484e3c913fe
justl0rd <marmorela@ukr.net>
2020-05-01 20:35:44 +0300

    add front page

---
+++ src/js/index.js
@@ -0,0 +1,57 @@
+function miniCartStatus() {
+    const toggleBtn = document.getElementById("open-mini-cart");
+    const miniCart = document.getElementById("mini-cart");
+
+    toggleBtn.onclick = () => {
+        miniCart.classList.toggle("active");
+    }
+}
+
+function langStatus() {
+    const langBtn = document.getElementById("lang__box");
+    langBtn.onclick = (e) => {
+        langBtn.classList.toggle("active");
+    }
+}
+
+function hamburgerStatus() {
+    const hamburgerBtn = document.getElementById("hamburger");
+    const hamburgerMenu = document.getElementById("menu");
+
+    hamburgerBtn.onclick = () => {
+        hamburgerBtn.classList.toggle("active");
+        hamburgerMenu.classList.toggle("active")
+    }
+}
+
+function modalClose(elem, className) {
+    elem.classList.remove(className);
+}
+
+function clickHandler() {
+    const hamburgerBtn = document.getElementById("hamburger");
+    const hamburgerMenu = document.getElementById("menu");
+    const miniCart = document.getElementById("mini-cart");
+    const miniCartBtn = document.getElementById("open-mini-cart");
+
+    document.addEventListener('click', (e) => {
+
+        if (e.target.closest('.hamburger') === hamburgerBtn || e.target.closest('#menu') === hamburgerMenu) {
+        } else {
+            modalClose(hamburgerBtn, "active");
+            modalClose(hamburgerMenu, "active");
+        }
+
+        if (e.target.closest('.mini-cart') === miniCart || e.target.closest(".cart-small") === miniCartBtn) {
+        } else {
+            modalClose(miniCart, "active");
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    miniCartStatus();
+    langStatus();
+    hamburgerStatus();
+    clickHandler();
+});
---
+++ src/js/owl-config.js
@@ -0,0 +1,10 @@
+$('.banner__slider').owlCarousel({
+    loop:true,
+    margin: 0,
+    nav: false,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+});
