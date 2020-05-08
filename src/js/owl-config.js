ff41d556f06453aada4e7fa7d122d4d49d2286f8
justl0rd <marmorela@ukr.net>
2020-05-08 16:42:04 +0300

    add similar products

--- src/js/owl-config.js
+++ src/js/owl-config.js
@@ -8,3 +8,32 @@ $('.banner__slider').owlCarousel({
         }
     }
 });
+
+$('.similar__slider').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:8000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        900:{
+            items:3
+        },
+        1200:{
+            items:4
+        },
+        1350:{
+            items:5
+        }
+    }
+});
+
+
